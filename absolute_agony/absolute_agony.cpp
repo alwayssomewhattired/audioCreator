512846dff6ea7e73b209c613918affb8327cebd9
Zachary Malinka <zacharymalinka@gmail.com>
2025-02-24 17:58:04 -0500

    websockets are set up and are able to communicate with python.

--- absolute_agony/absolute_agony.cpp
+++ absolute_agony/absolute_agony.cpp
@@ -35,6 +35,8 @@
 
 // add a normalisation feature that compresses the audio.
 
+// It takes a while to fetch audio from s3 and make the audio and then read the audio... more efficieny would be great.
+
 
 
 
@@ -44,7 +46,7 @@
 #define SERVER_IP "127.0.0.1"
 
 
-int websocket()
+int websocket(std::vector<double> sampleStorage)
 {
 	// Initialize Winsock
 	WSADATA wsaData;
@@ -61,7 +63,6 @@ int websocket()
 		WSACleanup();
 		return 1;
 	}
-
 	// Server address structure
 	sockaddr_in server_addr{};
 	server_addr.sin_family = AF_INET;
@@ -74,6 +75,17 @@ int websocket()
 		closesocket(sock);
 		WSACleanup();
 		return 1;
+	}
+
+	if (sampleStorage.size() >= 720000) {
+		const char* message = "finish_now";
+		send(sock, message, strlen(message), 0);
+	
+	// Cleanup
+	closesocket(sock);
+	WSACleanup();
+
+	return 0;
 	}
 
 	// Send command to Python server
@@ -96,12 +108,19 @@ int websocket()
 		std::cerr << "Failed to receive message\n";
 	}
 
-	// Cleanup
-	closesocket(sock);
-	WSACleanup();
+	//if (sampleStorage.size() > 720000) {
 
+	//	message = "finish_now";
+	//	send(sock, message, strlen(message), 0);
+	//	std::cout << "END" << std::endl;
+	//}
+
+	//	// Cleanup
+	//	closesocket(sock);
+	//	WSACleanup();
+
+	//return 0;
 	return 0;
-
 }
 
 
@@ -157,7 +176,7 @@ std::vector<double> read_audio_file(const std::string& filename)
 
 
 
-// create function to apply a hanning window 
+// create function to apply a hanning window s
 void applyHanningWindow(std::vector<double>& signal, int n) {
 	for (int i = 0; i < n; i++) {
 		double hannValue = 0.5 * (1 - cos(2 * M_PI * i / (n - 1)));
@@ -237,130 +256,137 @@ void ReadAudioFileFromS3(const Aws::String& bucketName, const Aws::String& objec
 
 int main()
 {
-	websocket();
-	const Aws::String bucketName = "firstdemoby";
-	const Aws::String objectKey = "fetch-test.mp3";
 
-	//ReadAudioFileFromS3(bucketName, objectKey);
-
-	std::string filename = "C:/Users/zacha/Desktop/empty/DeathGrips-Exmilitary-1-Beware.mp3";
-	std::vector<double> audio_data = read_audio_file(filename);
-	//std::vector<double> audio_data = read_audio_file("temp_audio.mp3"); // This is for AWS fetched
-	int n = audio_data.size();
-	std::cout << n << std::endl;
-	if (audio_data.empty()) {
-		return 1;
-	}
-
-	// make analysis output file
-	//std::ofstream outputFile("new_fft.txt");
-	std::ofstream magFile("mag.txt");
+		const Aws::String bucketName = "firstdemoby";
+		const Aws::String objectKey = "fetch-test.mp3";
 
 
+		//std::string filename = "C:/Users/zacha/Desktop/empty/DeathGrips-Exmilitary-1-Beware.mp3";
+		//std::vector<double> audio_data = read_audio_file(filename);
 
-	const char* outputName = "output.wav";
-	int sampleRate = 44100;
-	int channels = 1;
-	int format = SF_FORMAT_WAV | SF_FORMAT_PCM_32; // 32 bit pcm wav
+		const char* outputName = "output.wav";
+		int sampleRate = 44100;
+		int channels = 1;
+		int format = SF_FORMAT_WAV | SF_FORMAT_PCM_32; // 32 bit pcm wav
 
-	SNDFILE* infiniteFile;
-	SF_INFO sf_info;
-	sf_info.samplerate = sampleRate;
-	sf_info.channels = channels;
-	sf_info.format = format;
-
-	int chunk_size = 2048;
-
-	applyHanningWindow(audio_data, n);
-
-	int num_chunks = (n + chunk_size - 1) / chunk_size; // calculate all the number of chunks
+		SNDFILE* infiniteFile;
+		SF_INFO sf_info;
+		sf_info.samplerate = sampleRate;
+		sf_info.channels = channels;
+		sf_info.format = format;
+		int chunk_size = 2048;
 
 
-	// allocate memory for the real input and complex output of this chunk
-	int fft_size = chunk_size / 2 + 1; // size of complex output for this chunk
-	double* real_input = fftw_alloc_real(chunk_size); // real input array
-	if (real_input == nullptr) { // handle allocation error
-		std::cerr << "Failed to allocate real memory" << std::endl;
-		exit(1);
-	}
-	fftw_complex* complex_output = fftw_alloc_complex(fft_size); // complex output array
-	if (complex_output == nullptr) { // handle allocation error
-		std::cerr << "Failed to allocated complex memory" << std::endl;
-	}
+	while (sampleStorage.size() < 720000) {
+		std::cout << sampleStorage.size() << std::endl;
+		websocket(sampleStorage);
+		ReadAudioFileFromS3(bucketName, objectKey);
+		std::vector<double> audio_data = read_audio_file("temp_audio.mp3"); // This is for AWS fetched
+		int n = audio_data.size();
+		std::cout << n << std::endl;
+		if (audio_data.empty()) {
+			return 1;
+		}
 
-	// create fftw plan for this chunk
-	fftw_plan plan = fftw_plan_dft_r2c_1d(chunk_size, real_input, complex_output, FFTW_MEASURE);
+		// make analysis output file
+		//std::ofstream outputFile("new_fft.txt");
+		//std::ofstream magFile("mag.txt");
 
-	// create vector for magnitude storage
-	std::vector<double> magnitudes(chunk_size);
+		int num_chunks = (n + chunk_size - 1) / chunk_size; // calculate all the number of chunks
 
-	int counter = -1;
+		applyHanningWindow(audio_data, n);
 
-	// loop over each chunk
-	for (int chunk = 0; chunk < num_chunks - 1; ++chunk) { // num_chuks is strange
-		++counter;
-		std::cout << "chunk: " << chunk << std::endl;
-		std::cout << "num chunk " << num_chunks << std::endl;
-		//std::cout << "counter: " << counter << std::endl;
-		// Determine the starting and ending index for the chunk
-		int start = chunk * chunk_size;
-		int end = std::min<int>(start + chunk_size, n); // added <int> identifier
+		// allocate memory for the real input and complex output of this chunk
+		int fft_size = chunk_size / 2 + 1; // size of complex output for this chunk
+		double* real_input = fftw_alloc_real(chunk_size); // real input array
+		if (real_input == nullptr) { // handle allocation error
+			std::cerr << "Failed to allocate real memory" << std::endl;
+			exit(1);
+		}
+		fftw_complex* complex_output = fftw_alloc_complex(fft_size); // complex output array
+		if (complex_output == nullptr) { // handle allocation error
+			std::cerr << "Failed to allocated complex memory" << std::endl;
+		}
+
+		// create fftw plan for this chunk
+		fftw_plan plan = fftw_plan_dft_r2c_1d(chunk_size, real_input, complex_output, FFTW_MEASURE);
+
+		// create vector for magnitude storage
+		std::vector<double> magnitudes(chunk_size);
+
+		int counter = -1;
+
+		// loop over each chunk
+		for (int chunk = 0; chunk < num_chunks - 1; ++chunk) { // num_chuks is strange
+			++counter;
+			std::cout << "chunk: " << chunk << std::endl;
+			std::cout << "num chunk " << num_chunks << std::endl;
+			//std::cout << "counter: " << counter << std::endl;
+			// Determine the starting and ending index for the chunk
+			int start = chunk * chunk_size;
+			int end = std::min<int>(start + chunk_size, n); // added <int> identifier
 
 
-		// copy the audio source data into the real_input array
-		std::fill(real_input, real_input + chunk_size, 0); // Zero out the input
-		std::copy(audio_data.begin() + start, audio_data.begin() + end, real_input);
+			// copy the audio source data into the real_input array
+			std::fill(real_input, real_input + chunk_size, 0); // Zero out the input
+			std::copy(audio_data.begin() + start, audio_data.begin() + end, real_input);
 
-		//execute the fft
-		fftw_execute(plan);
+			//execute the fft
+			fftw_execute(plan);
 
-		// open file to store analysis
-		//if (outputFile.is_open()) {
-				// process the complex output
-				for (int i = 0; i < fft_size; ++i) {
-					//std::cout << "samples: " << i << " " << audio_data[i + chunk_size] << std::endl;
-					double freq = static_cast<double>(i) * sampleRate / chunk_size;
-					magnitudes[i] = std::sqrt(complex_output[i][0] * complex_output[i][0]
-						+ complex_output[i][1] * complex_output[i][1]);
-					// push freq/bins/magnitude to text file
-					//outputFile << "bin " << i << ": Frequency = " << freq << " Hz, magnitude = "
-						//<< magnitudes[i] << "\n";
-				}
+			// open file to store analysis
+			//if (outputFile.is_open()) {
+					// process the complex output
+			for (int i = 0; i < fft_size; ++i) {
+				//std::cout << "samples: " << i << " " << audio_data[i + chunk_size] << std::endl;
+				double freq = static_cast<double>(i) * sampleRate / chunk_size;
+				magnitudes[i] = std::sqrt(complex_output[i][0] * complex_output[i][0]
+					+ complex_output[i][1] * complex_output[i][1]);
+				// push freq/bins/magnitude to text file
+				//outputFile << "bin " << i << ": Frequency = " << freq << " Hz, magnitude = "
+					//<< magnitudes[i] << "\n";
+			}
 
 			//check magnitudes for wanted frequencies
 			isGreaterThanAll(magnitudes, magnitudes[23], counter, audio_data, chunk_size);
-		//} // curly for output file
+			//} // curly for output file
 
-	}
-	// I wonder if this is supposed to be in the for loop.
-	//outputFile.close();
-
-	if (magFile.is_open()) {
-		for (double element : sampleStorage) {
-			magFile << "sample: " << element << std::endl;
 		}
+		// I wonder if this is supposed to be in the for loop.
+		//outputFile.close();
+
+	/*	if (magFile.is_open()) {
+			for (double element : sampleStorage) {
+				magFile << "sample: " << element << std::endl;
+			}
+		}
+		magFile.close();*/
+
+
+		if (sampleStorage.size() >= 720000) {
+			// open wav file for writing
+			infiniteFile = sf_open(outputName, SFM_WRITE, &sf_info);
+			if (!infiniteFile) {
+				std::cerr << "error opening file: " << outputName << std::endl;
+				return 1;
+			}
+
+			// write data for writing wav file
+			sf_count_t framesWritten = sf_writef_double(infiniteFile, sampleStorage.data(), sampleStorage.size()); // might need 'sizeof' for size
+			if (framesWritten != sampleStorage.size()) {
+				std::cerr << "Error writing data: " << sf_strerror(infiniteFile) << std::endl;
+			}
+
+			sf_close(infiniteFile);
+		}
+
+		//clean up
+		fftw_destroy_plan(plan);
+		fftw_free(real_input);
+		fftw_free(complex_output);
+
 	}
-	magFile.close();
+	websocket(sampleStorage);
 
-	// open wav file for writing
-	infiniteFile = sf_open(outputName, SFM_WRITE, &sf_info);
-	if (!infiniteFile) {
-		std::cerr << "error opening file: " << outputName << std::endl;
-		return 1;
-	}
-
-	// write data for writing wav file
-	sf_count_t framesWritten = sf_writef_double(infiniteFile, sampleStorage.data(), sampleStorage.size()); // might need 'sizeof' for size
-	if (framesWritten != sampleStorage.size()) {
-		std::cerr << "Error writing data: " << sf_strerror(infiniteFile) << std::endl;
-	}
-
-	sf_close(infiniteFile);
-
-	//clean up
-	fftw_destroy_plan(plan);
-	fftw_free(real_input);
-	fftw_free(complex_output);
-	
 	std::cout << "FINISHED" << std::endl;
 }
